24f8cbb31c226fb16aad60255f68ca2a71af3b9e
Raull888 <hla055030@gmail.com>
2025-05-05 22:46:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const userRoutes = require(path.join(__dirname, 'routes', 'userRoutes'));
-const postRoutes = require('./routes/postRoutes');
-const ratingRoutes = require('./routes/ratingRoutes');
-const db = require('../config/db');
-
 dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
 
-// Conectar rutas
-app.use('/api/users', userRoutes);
-app.use('/api/posts', postRoutes);
-app.use('/api/ratings', ratingRoutes);
+// Rutas
+app.get('/', (req, res) => {
+  res.send('Backend está funcionando');
+});
 
-// Iniciar servidor
-app.listen(process.env.PORT, () => {
-  console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
+app.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
 });
